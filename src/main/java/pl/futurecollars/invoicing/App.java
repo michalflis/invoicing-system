2ff1d683bcb235f2c8cc3ee61d2d952d97f95f89
michalflis <michalt.flis@gmail.com>
2021-08-27 18:28:36 +0200

    configuration of JaCoCo, Github actions and Lombok

--- src/main/java/pl/futurecollars/invoicing/App.java
+++ src/main/java/pl/futurecollars/invoicing/App.java
@@ -1,12 +1,15 @@
 package pl.futurecollars.invoicing;
 
+import lombok.Data;
+
+@Data
 public class App {
 
-  public String getGreeting() {
-    return "Hello world.";
-  }
+    public String getGreeting() {
+        return "Hello world.";
+    }
 
-  public static void main(String[] args) {
-    System.out.println(new App().getGreeting());
-  }
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
 }
